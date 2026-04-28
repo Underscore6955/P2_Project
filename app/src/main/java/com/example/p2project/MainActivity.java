0ffcefa967d8ada75ab7d3120a581b4dd54d1f8a
Underscore6955 <mikkelrmarquardsen@gmail.com>
2026-04-28 09:36:43 +0200

    Made sure only 1 set of animals are made

--- app/src/main/java/com/example/p2project/MainActivity.java
+++ app/src/main/java/com/example/p2project/MainActivity.java
@@ -15,7 +15,7 @@ import com.example.p2project.IdleGame.Inventory.ActiveSlot;
 import com.example.p2project.IdleGame.Inventory.InventorySlot;
 
 public class MainActivity extends AppCompatActivity {
-
+    static Boolean invStarted;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,17 +24,21 @@ public class MainActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            InsertButtons();
-            Inventory.inventory.add(new Animal("Bear",1000f));
-            Inventory.inventory.add(new Animal("Cat",1000f));
-            Inventory.inventory.add(new Animal("Chameleon",1000f));
-            Inventory.inventory.add(new Animal("Chinchilla",1000f));
-            Inventory.inventory.add(new Animal("Crab",1000f));
-            Inventory.inventory.add(new Animal("Donkey",1000f));
-            Inventory.inventory.add(new Animal("Fox",1000f));
-            Inventory.inventory.add(new Animal("Jellyfish",1000f));
-            Inventory.inventory.add(new Animal("Panda",1000f));
-            Inventory.UpdateInventory();
+            if (!invStarted)
+            {
+                InsertButtons();
+                Inventory.inventory.add(new Animal("Bear",1000f));
+                Inventory.inventory.add(new Animal("Cat",1000f));
+                Inventory.inventory.add(new Animal("Chameleon",1000f));
+                Inventory.inventory.add(new Animal("Chinchilla",1000f));
+                Inventory.inventory.add(new Animal("Crab",1000f));
+                Inventory.inventory.add(new Animal("Donkey",1000f));
+                Inventory.inventory.add(new Animal("Fox",1000f));
+                Inventory.inventory.add(new Animal("Jellyfish",1000f));
+                Inventory.inventory.add(new Animal("Panda",1000f));
+                Inventory.UpdateInventory();
+                invStarted = true;
+            }
             return insets;
         });
     }
